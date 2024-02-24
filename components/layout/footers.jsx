17803d6b13799843751264977a82171ad12a14d7
BToan12 <105841085+BToan12@users.noreply.github.com>
2024-02-24 10:35:37 +0700

    up

---
+++ components/layout/footers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footers() {
+  return (
+    <footer className='footer'>
+      <h2>Hotel Room Booking System</h2>
+    </footer>
+  );
+}
+
+export default Footers;
---
+++ store/slices/appSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState: {
+    timeZone: '',
+    isLoading: false,
+    reFetch: false,
+    theme: {
+      colorPrimary: '#4096ff',
+      colorLink: '#ececec'
+    }
+  },
+  reducers: {
+    setLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+    reFetchData: (state) => {
+      state.reFetch = !state.reFetch;
+    }
+  }
+});
+
+export const { setLoading, reFetchData } = appSlice.actions;
+
+export default appSlice.reducer;
